c0ecdab296d14af917ba6ac78d48be19d2064495
krenko <krenkocoin@gmail.com>
2025-09-22 19:41:00 -0300

    Removendo todos os arquivos para recomeçar

--- assets/js/gamificacao.js
+++
@@ -1,9 +0,0 @@
-// Aqui você pode evoluir pontos, badges etc.
-// Por exemplo:
-let pontos = Number(localStorage.getItem("userScore")) || 0;
-
-function adicionarPontos(qtd) {
-  pontos += qtd;
-  localStorage.setItem("userScore", pontos);
-  console.log("🏆 Pontos:", pontos);
-}
